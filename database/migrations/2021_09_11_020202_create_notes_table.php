1f0fcc6c16befcca3d69c6472a3dcef02aa1cd0d
alicaputriazz <alicaputriazz@gmail.com>
2021-09-11 09:16:32 +0700

    Making Database Structure

---
+++ database/migrations/2021_09_11_020202_create_notes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('subject_id')->unsigned()->index()->nullable();
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
