4368138b3f1b8de3d10864e6a031cf3e9710d8b0
alicaputriazz <alicaputriazz@gmail.com>
2021-09-13 19:15:07 +0700

    trying to make note api

---
+++ resources/js/views/notes/Create.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">New Note</div>
+                    <div class="card-body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Create Post"
+};
+</script>
+
+<style></style>
